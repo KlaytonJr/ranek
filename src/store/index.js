36538d942b2822afe532b69f01c4e6a788d365c3
Klayton Junior <klaytonjunior@protonmail.com>
2021-05-12 22:27:12 -0300

    commit ranek v1.0

--- src/store/index.js
+++ src/store/index.js
@@ -1,14 +1,77 @@
 import Vue from 'vue'
 import Vuex from 'vuex'
+import { api } from "@/services.js"
 
 Vue.use(Vuex)
 
 export default new Vuex.Store({
+  strict: true,
   state: {
+    login: false,
+    usuario: {
+      id: "",
+      nome: "",
+      email: "",
+      senha: "",
+      cep: "",
+      rua: "",
+      numero: "",
+      bairro: "",
+      cidade: "",
+      estado: ""
+    },
+    usuario_produtos: null
   },
   mutations: {
+    UPDATE_LOGIN(state, payload) {
+      state.login = payload;
+    },
+    UPDATE_USUARIO(state, payload) {
+      state.usuario = Object.assign(state.usuario, payload);
+      //state.usuario = Object.assign({}, state.usuario, payload);
+      // Combinar com um vazio, pois caso algum elemento seja null ele não fique null
+    },
+    UPDATE_USUARIO_PRODUTOS(state, payload) {
+      state.usuario_produtos = payload;
+    },
+    ADD_USUARIO_PRODUTOS(state, payload) {
+      state.usuario_produtos.unshift(payload);
+      // Usa o unshift para as últimas adições vim primeiro
+    }
   },
   actions: {
+    getUsuario(context, payload) {
+      api.get(`/usuario/${payload}`)
+        .then(response => {
+          context.commit("UPDATE_USUARIO", response.data);
+          context.commit("UPDATE_LOGIN", true);
+        })
+    },
+    criarUsuario(context, payload) {
+      context.commit("UPDATE_USUARIO", {id: payload.email})
+      api.post(`/usuario`, payload);
+    },
+    deslogarUsuario(context) {
+      context.commit("UPDATE_USUARIO", {
+        id: "",
+        nome: "",
+        email: "",
+        senha: "",
+        cep: "",
+        rua: "",
+        numero: "",
+        bairro: "",
+        cidade: "",
+        estado: ""
+      });
+      context.commit("UPDATE_LOGIN", false);
+    },
+    getUsuarioProdutos(context) {
+      api.get(`/produto?usuario_id=${context.state.usuario.id}`)
+        .then(response => {
+          context.commit("UPDATE_USUARIO_PRODUTOS", response.data);
+        })
+    }
   },
   modules: {
   }
