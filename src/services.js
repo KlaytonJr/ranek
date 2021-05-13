d5f2496d257d58be475ca75467b5ee40e8ca8eac
Klayton Junior <klaytonjunior@protonmail.com>
2021-05-13 09:09:46 -0300

    change api route

--- src/services.js
+++ src/services.js
@@ -1,7 +1,7 @@
 import axios from "axios";
 
 const axiosInstance = axios.create({
-    baseURL: "http://localhost:3000"
+    baseURL: "https://my-json-server.typicode.com/KlaytonJr/ranek-api"
 });
 
 export const api = {
