37ca68b0dc6fbb8856d5d2ab12c60fc0fc0f41c2
Klayton Junior <klaytonjunior@protonmail.com>
2021-05-13 13:48:06 -0300

    update v1.0

--- src/services.js
+++ src/services.js
@@ -1,5 +1,9 @@
 import axios from "axios";
 
+// const axiosInstance = axios.create({
+//     baseURL: "http://localhost:3000"
+// });
+
 const axiosInstance = axios.create({
     baseURL: "https://my-json-server.typicode.com/KlaytonJr/ranek-api"
 });
